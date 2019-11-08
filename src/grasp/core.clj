8e5692234ea47843f4f35c17c0b3311ab2401890
Éden Ferreira <thiago_eden@hotmail.com>
2019-11-08 09:57:30 -0300

    Documenting

--- src/grasp/core.clj
+++ src/grasp/core.clj
@@ -52,6 +52,19 @@
        (throw (:exception result#)))))
 
 (defmacro grab-call
+  "Grabs the call and add its execution to the log.
+   It knows how to deal with exceptions, and adds to the log
+   the metadata of the fn being call, the var, the args that were
+   passed as parameter and the result. If it is a pure function (it should)
+   you can replay exactly what happened and than deep dive
+   in that function with more sparse (or not) grabs to pinpoint the
+   problem that you are trying to find.
+   Beside the form you can pass a grab-id as the first parameter,
+   and if you so choose even the atom and execution id atoms, but
+   totally optional and you would be better just using the global
+   ones.
+   Grab-id and execution-id and not that special, are just to help
+   filter the log to find what you want when the log gets chaotic."
   ([form]
    (emit-grab-call nil form log current-execution-id))
   ([grab-id form]
@@ -71,6 +84,16 @@
      (:value result#)))
 
 (defmacro grab-value
+  "Grabs the value and add it to the log.
+   It knows how to deal with exceptions, and adds to the log
+   the metadata of the fn being call, the var, the args that were
+   passed as parameter and the result.
+   Beside the form you can pass a grab-id as the first parameter,
+   and if you so choose even the atom and execution id atoms, but
+   totally optional and you would be better just using the global
+   ones.
+   Grab-id and execution-id and not that special, are just to help
+   filter the log to find what you want when the log gets chaotic."
   ([form]
    (emit-grab-value nil form log current-execution-id))
   ([grab-id form]
@@ -84,9 +107,41 @@
     (emit-grab-value grab-id form log execution-id)))
 
 (defmacro grab
+  "It chooses if should grab as a call or a value,
+   to see what each does see grab-call and grab-value docs"
   ([form]
    (emit-grab nil form log current-execution-id))
   ([grab-id form]
    (emit-grab grab-id form log current-execution-id))
   ([grab-id form & {:keys [log execution-id]}]
    (emit-grab grab-id form log execution-id)))
+
+(defmacro ->grab-call
+  "see grab-call docs"
+  [form grab-id]
+  (emit-grab-call grab-id form log current-execution-id))
+
+(defmacro ->>grab-call
+  "see grab-call docs"
+  [grab-id form]
+  (emit-grab-call grab-id form log current-execution-id))
+
+(defmacro ->grab-value
+  "see grab-value docs"
+  [form grab-id]
+  (emit-grab-value grab-id form log current-execution-id))
+
+(defmacro ->>grab-value
+  "see grab-value docs"
+  [grab-id form]
+  (emit-grab-value grab-id form log current-execution-id))
+
+(defmacro ->grab
+  "see grab docs"
+  [form grab-id]
+  (emit-grab grab-id form log current-execution-id))
+
+(defmacro ->>grab
+  "see grab docs"
+  [grab-id form]
+  (emit-grab grab-id form log current-execution-id))
